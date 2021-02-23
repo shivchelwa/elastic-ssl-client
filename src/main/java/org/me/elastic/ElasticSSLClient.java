a8c0f9de3db7b20dd3f8120a0b96d7abc102a87c
shivchelwa <shivkumar.chelwa@gmail.com>
2021-02-23 09:31:33 -0500

    Allow empty truststore password

--- src/main/java/org/me/elastic/ElasticSSLClient.java
+++ src/main/java/org/me/elastic/ElasticSSLClient.java
@@ -9,10 +9,13 @@ import java.security.KeyStore;
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
-import org.elasticsearch.action.support.ActiveShardCount;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -38,31 +41,46 @@ public class ElasticSSLClient {
 	}
 	
 	private static RestHighLevelClient connect(String pathToCertStore, String keyStorePass) throws Exception {
+
+		RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "https"));
+
+		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "elastic"));
+		builder = builder.setHttpClientConfigCallback(
+				httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+//		String apiKey = "_0jky3cBR4P_5_33oEnw";
+//		String apiSecret = "xEAxS6TDRhuSYQiOIH6PlA";
+//		String apiKeyAuth = Base64.getEncoder()
+//				.encodeToString((apiKey + ":" + apiSecret).getBytes(StandardCharsets.UTF_8));
+//		Header[] defaultHeaders = new Header[] { new BasicHeader("Authorization", "ApiKey " + apiKeyAuth) };
+//		builder = builder.setDefaultHeaders(defaultHeaders);
+		
 		Path trustStorePath = Paths.get(pathToCertStore);
 		KeyStore truststore = KeyStore.getInstance("pkcs12");
 		try (InputStream is = Files.newInputStream(trustStorePath)) {
 			if (keyStorePass != null && !keyStorePass.isBlank()) {
 				truststore.load(is, keyStorePass.toCharArray());
 			} else {
-				truststore.load(is, null);
+				truststore.load(is, "".toCharArray());
 			}
 		}
 		SSLContextBuilder sslBuilder = SSLContexts.custom().loadTrustMaterial(truststore, null);
 		final SSLContext sslContext = sslBuilder.build();
-		RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "https"))
-				.setHttpClientConfigCallback(new HttpClientConfigCallback() {
-					@Override
-					public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-						return httpClientBuilder.setSSLContext(sslContext);
-					}
-				});
+		builder = builder.setHttpClientConfigCallback(new HttpClientConfigCallback() {
+						@Override
+						public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+							return httpClientBuilder.setSSLContext(sslContext);
+						}
+					});
+		
 		return new RestHighLevelClient(builder);
 
 	}
 
 	private static void createIndexes(RestHighLevelClient elasticClient) throws Exception {
 		boolean indexExist = false;
-		String indexName = "test_index";
+		String indexName = "test_index_1";
 
 		GetIndexRequest getIndexReq = new GetIndexRequest(indexName);
 		getIndexReq.setTimeout(TimeValue.timeValueMillis(10000));
@@ -79,13 +97,14 @@ public class ElasticSSLClient {
 
 			createIndexReq.setTimeout(TimeValue.timeValueMillis(10000));
 			createIndexReq.setMasterTimeout(TimeValue.timeValueMillis(10000));
-			createIndexReq.waitForActiveShards(ActiveShardCount.from(10000));
 			CreateIndexResponse createIndexResponse = elasticClient.indices().create(createIndexReq,
 					RequestOptions.DEFAULT);
 			if (!createIndexResponse.isAcknowledged())
 				throw new Exception(String.format("Error creating index[%s] for entity[%s]",
 						createIndexResponse.index(), indexName));
 
+		} else {
+			System.out.println("Index exist..");
 		}
 	}
 }
